42e2a8d0db2605567f6a8bad960621db10556b94
Aengus McMillin <aengusmcmillin@gmail.com>
2020-04-02 19:44:25 -0700

    Add musical balance post and get youtube embeds working

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,6 +57,14 @@ module.exports = {
               maxWidth: 1380,
               linkImagesToOriginal: false
             }
+          },
+          {
+            resolve: "gatsby-remark-embed-video",
+            options: {
+              width: '560',
+              related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
+              noIframeBorder: true, //Optional: Disable insertion of <style> border: 0
+            }
           }
         ]
       }
